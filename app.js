672f7fc3270ace4195b3450784ee91c9e562c016
XinlaiHe <xinlai_he@epam.com>
2016-07-02 20:54:32 +0800

    extract express

--- app.js
+++ app.js
@@ -1,38 +1,4 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require("./config/mongoose.config");
-var passport = require("./config/passport.config");
-var LocalStrategy = require('passport-local').Strategy;
-var session = require('express-session');
-var flash = require('express-flash');
-
-var app = express();
-
-//Initialize passport
-app.use(express.static('public'));
-app.use(cookieParser());
-app.use(bodyParser.json('2mb'));
-app.use(bodyParser.urlencoded({extended: true, limit: '2mb'}));
-app.use(session({ secret: 'keyboard cat' , resave: false, saveUninitialized: false}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-var users = require('./routes/user.route');
-
-// Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
-
-app.use('/users', users);
+var app = require('./config/express.config.js');
 
 app.listen(8000, function(err){
   if(err) console.log(err)
