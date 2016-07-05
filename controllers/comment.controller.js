eab1b84ecb1719271bdc02ad5a3b56707b6b5b81
Claire Liu <wenjing.liu09@foxmail.com>
2016-07-05 15:52:43 +0800

    add  api comments/articles/:userId

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('../config/mongoose.config');
+const ObjectId = require('mongoose').Types.ObjectId;
 // register the comment schema
 require('../models/comment.model');
 var Comment = mongoose.model("Comment");
+var Article = mongoose.model("Article");
 
 exports.add = function(req, res){
 	console.log(req.body);
@@ -131,6 +133,57 @@ exports.readByUserId = function (req, res, next){
 			}
 		});
 }
+
+exports.readArticlesByUserId = function (req, res, next){
+  var userId = req.params.userId;
+  // console.log(userId);
+  var articleCommentedByUser = [];
+  var articles = [];
+  Comment.find({}, function(err, comments){
+    // console.log(comments);
+    if(err){
+      res.jsonp(err);
+    }else{
+      for(let i = 0; i < comments.length; i++){
+        for(let m = 0; m < comments[i].comments.length; m++){
+          if(comments[i].comments[m].creator === userId){
+            articleCommentedByUser.push(comments[i].articleId);
+            break;
+          }
+          let comments2comments = comments[i].comments[m].comments2comments;
+          for(let j = 0; j < comments2comments.length; j++){
+            if(comments2comments[j].replyer === userId){
+              articleCommentedByUser.push(comments[i].articleId);
+              break;
+            }
+          }
+        }
+      }
+      // console.log(articleCommentedByUser);
+      for(let n = 0; n < articleCommentedByUser.length; n++){
+        console.log(articleCommentedByUser[n]);
+        Article.findOne({ _id: new ObjectId(articleCommentedByUser[n]) },function(err, article){
+          if(article !== null){
+            articles.push({
+              id: article._id,
+              title: article.title,
+              author: article.author,
+              topic: article.topic,
+              publishDate: article.publishDate
+            });
+          }
+/*          console.log(articles);
+          console.log(articles.length);
+          console.log(articleCommentedByUser.length);*/
+          if(articles.length === articleCommentedByUser.length){
+            res.jsonp(articles);
+          }
+        });
+      }
+    }
+  });
+}
+
 
 
 /*
